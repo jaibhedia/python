828c95bde77a1ba8a79c38b0d8dac178978be534
jaibhedia <jaibhedia@gmail.com>
2023-09-22 00:17:16 +0530

    update

--- 066_5b.py
+++ 066_5b.py
@@ -1,13 +1,22 @@
 import re
-phone_regex=re.compile(r'\+\d{12}')
-email_regex=re.compile(r'[A-Za-z0-9._]+@[A-Za-z0-9]+\.[A-Z|a-z]{2,}')
-date_regex=re.compile(r'[0-9]+-[0-9]+-[0-9]{3,}')
-with open('kar.txt', 'r') as f:
+
+phone_regex = re.compile(r'\+\d{12}')
+email_regex = re.compile(r'[A-Za-z0-9._]+@[A-Za-z0-9]+\.[A-Z|a-z]{2,}')
+date_regex = re.compile(r'[0-9]+-[0-9]+-[0-9]{3,}')
+
+# Open the input file (EXAMPLE.TXT in this case)
+with open('EXAMPLE.TXT', 'r') as f:
     for line in f:
-        matches=phone_regex.findall(line)
-        match_email=email_regex.findall(line)
-        match_date=date_regex.findall(line)
-        for phone,email in zip(matches,match_email):
-            print(phone,email)
-        for dates in zip(match_date):
-            print(match_date)
+        line = line.strip()  # Remove leading/trailing whitespace
+        matches = phone_regex.findall(line)
+        match_email = email_regex.findall(line)
+        match_date = date_regex.findall(line)
+
+        # Join the matches into a single string separated by tabs
+        phone_str = "\t".join(matches)
+        email_str = "\t".join(match_email)
+        
+        print(f"{line}\t{email_str}\t{phone_str}")
+
+        for date in match_date:
+            print(f"\t\t\t{date}")
